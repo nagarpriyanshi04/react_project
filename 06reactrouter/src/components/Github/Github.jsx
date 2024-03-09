313659cd831cd9ba8f039238969e915390c312e9
nagarpriyanshi04 <priyanshin14@gmail.com>
2024-03-10 00:20:57 +0530

    react router

---
+++ 06reactrouter/src/components/Github/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect,useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data,setData] = useState([])
+    // useEffect (() => {
+    //     fetch('https://api.github.com/users/nagarpriyanshi04')
+    //     .then (response => response.json())
+    //     .then (data => {
+    //         setData(data)
+    //     })
+
+    // })
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers: {data.followers}
+      <img src="{data.avatar_url}" width={300}/>
+    </div>
+
+  )
+}
+
+export default Github
+
+
+//in next file
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/nagarpriyanshi04')
+    return response.json()
+}
